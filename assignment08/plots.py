a0b31ad48ee7dfb7124767af265801fa6e0d5f87
Ieva <kerseviciute.ieva@gmail.com>
2025-01-29 19:07:02 +0100

    Clean up code, include subject comments.

--- assignment08/plots.py
+++ assignment08/plots.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+import dimensions as dims
+import copy
 
 
 def get_experiment_colors(experiment):
@@ -128,3 +131,183 @@ def define_experiment():
     full_experiment = pd.DataFrame({ "Trial": attempts, "Type": types })
 
     return experiment, full_experiment
+
+
+def plot_ellipse_around_points(x, y, ax, color, nstd = 2, **kwargs):
+    cov = np.cov(x, y)
+    pos = (np.mean(x), np.mean(y))
+
+    eigvals, eigvecs = np.linalg.eigh(cov)
+    order = eigvals.argsort()[::-1]
+    eigvals, eigvecs = eigvals[order], eigvecs[:, order]
+    angle = np.degrees(np.arctan2(*eigvecs[:, 0][::-1]))
+    width, height = 2 * nstd * np.sqrt(eigvals)
+
+    ellipse = Ellipse(xy = pos, width = width, height = height, angle = angle, **kwargs)
+
+    ax.scatter(pos[0], pos[1], marker = "x", color = color)
+    ax.add_patch(ellipse)
+
+
+def plot_throw_perturbation(subject):
+    default_colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+
+    min_pos_x = subject.EndPosX.min() - 100
+    max_pos_x = dims.screen_width + 50
+
+    min_pos_y = subject.EndPosY.min() - 100
+    max_pos_y = dims.screen_height + 50
+
+    fig, axs = plt.subplots(nrows = 2, ncols = 3, figsize = (12, 6))
+
+    legend_handles = []
+
+    for (i, feedback), ax in zip(enumerate(subject.Type.unique()), axs.flatten()[:-1]):  # Exclude the last subplot
+        screen_ = copy.deepcopy(dims.screen)
+        table_ = copy.deepcopy(dims.table)
+
+        ax.add_patch(screen_)
+        ax.add_patch(table_)
+        ax.fill(dims.green_x, dims.green_y, color = "green", alpha = 0.1)
+        ax.fill(dims.red_x, dims.red_y, color = "red", alpha = 0.1)
+
+        group_data = subject[subject.Type == feedback].reset_index(drop = True).sort_values(by = "Trial")
+        no_perturb_before = group_data.loc[:10]
+        perturb = group_data.loc[10:40]
+        no_perturb_after = group_data.loc[40:]
+
+        scatter_before = ax.scatter(
+            no_perturb_before.EndPosX,
+            no_perturb_before.EndPosY,
+            label = "No Perturbation (Before)",
+            c = default_colors[0],
+            s = 10
+        )
+        plot_ellipse_around_points(
+            no_perturb_before.EndPosX, no_perturb_before.EndPosY,
+            ax = ax,
+            color = default_colors[0],
+            alpha = 0.75,
+            edgecolor = default_colors[0],
+            facecolor = "none",
+            lw = 2
+        )
+
+        scatter_perturb = ax.scatter(
+            perturb.EndPosX,
+            perturb.EndPosY,
+            label = "Perturbation",
+            c = default_colors[1],
+            s = 10
+        )
+        plot_ellipse_around_points(
+            perturb.EndPosX, perturb.EndPosY,
+            ax = ax,
+            color = default_colors[1],
+            alpha = 0.75,
+            edgecolor = default_colors[1],
+            facecolor = "none",
+            lw = 2
+        )
+
+        scatter_after = ax.scatter(
+            no_perturb_after.EndPosX,
+            no_perturb_after.EndPosY,
+            label = "No Perturbation (After)",
+            c = default_colors[2],
+            s = 10
+        )
+        plot_ellipse_around_points(
+            no_perturb_after.EndPosX, no_perturb_after.EndPosY,
+            ax = ax,
+            color = default_colors[2],
+            alpha = 0.75,
+            edgecolor = default_colors[2],
+            facecolor = "none",
+            lw = 2
+        )
+
+        ax.set_xlim(min_pos_x, max_pos_x)
+        ax.set_ylim(min_pos_y, max_pos_y)
+
+        ax.set_title(feedback)
+        ax.invert_yaxis()
+
+        if i == 0:
+            legend_handles.extend([scatter_before, scatter_perturb, scatter_after, screen_, table_])
+
+    # Hide the 6th subplot and use it for the legend
+    axs[1, 2].axis("off")
+    axs[1, 2].legend(handles = legend_handles, loc = "center", frameon = False, title = "Legend")
+
+    fig.tight_layout()
+
+
+def plot_throw_positions(subject):
+    default_colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+
+    min_pos_x = subject.EndPosX.min() - 100
+    max_pos_x = dims.screen_width + 50
+
+    min_pos_y = subject.EndPosY.min() - 100
+    max_pos_y = dims.screen_height + 50
+
+    fig, axs = plt.subplots(nrows = 1, ncols = 2, figsize = (12, 4))
+
+    axs[0].add_patch(copy.deepcopy(dims.screen))
+    axs[1].add_patch(copy.deepcopy(dims.screen))
+    
+    axs[0].add_patch(copy.deepcopy(dims.table))
+    axs[1].add_patch(copy.deepcopy(dims.table))
+    
+    axs[0].fill(dims.green_x, dims.green_y, color = "green", alpha = 0.1)
+    axs[1].fill(dims.green_x, dims.green_y, color = "green", alpha = 0.1)
+    
+    axs[0].fill(dims.red_x, dims.red_y, color = "red", alpha = 0.1)
+    axs[1].fill(dims.red_x, dims.red_y, color = "red", alpha = 0.1)
+    
+    for i, feedback in enumerate(subject.Type.unique()):
+        group_data = subject[subject.Type == feedback]
+        plot_ellipse_around_points(
+            group_data.EndPosX, group_data.EndPosY,
+            ax = axs[0],
+            color = default_colors[i],
+            alpha = 0.75,
+            edgecolor = default_colors[i],
+            facecolor = "none",
+            lw = 2,
+            label = feedback
+        )
+    
+    for i, feedback in enumerate(subject.Type.unique()):
+        group_data = subject[subject.Type == feedback]
+        axs[1].scatter(
+            group_data.EndPosX,
+            group_data.EndPosY,
+            color = default_colors[i],
+            s = 5,
+            label = feedback
+        )
+    
+    axs[0].set_xlim(min_pos_x, max_pos_x)
+    axs[0].set_ylim(min_pos_y, max_pos_y)
+    
+    axs[1].set_xlim(min_pos_x, max_pos_x)
+    axs[1].set_ylim(min_pos_y, max_pos_y)
+    
+    axs[0].invert_yaxis()
+    axs[1].invert_yaxis()
+    
+    axs[1].legend(loc = "center left", bbox_to_anchor = (1, 0.5))
+    
+    axs[0].set_xlabel("X")
+    axs[0].set_ylabel("Y")
+    
+    axs[1].set_xlabel("X")
+    axs[1].set_ylabel("Y")
+    
+    axs[0].set_title("Distribution around mean throw position")
+    axs[1].set_title("All throw positions")
+    
+    fig.tight_layout()
+
